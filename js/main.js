d45c0fc94df3f84ab5e52ff3069fd477cd740151
Alexa Heitzenrader <alexaheitzenrader@Alexas-MacBook-Pro.local>
2017-11-27 09:45:29 -0800

    day 27

---
+++ js/main.js
@@ -0,0 +1,5 @@
+var canvas = document.getElementById("canvas");
+var context = canvas.getContext ("2d");
+context.font = "30px Georgia";
+context.fillText("This is JavaScript", 30, 80); //text and x and y axis
+
